c08f50122492b2871bcff13489220e6fdf09430f
Madhu Sudhanaan <careerformadhu@gmail.com>
2025-01-15 21:25:09 +0530

    S

--- index.py
+++ index.py
@@ -2,6 +2,6 @@ from flask import Flask
 helloworld = Flask(__name__)
 @helloworld.route("/")
 def run():
-    return "{\"message\":\"hi python \"}" 
+    return "{\"message\":\"hi python app\"}" 
 if __name__ == "__main__":
     helloworld.run(host="0.0.0.0",port=int("3000"),debug=True)%                                                                                                                                                                                                               madhusudhanan@madhus-MacBook-Air docker-sample % 
